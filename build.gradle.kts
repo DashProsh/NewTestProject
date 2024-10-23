793515e33a5ed756d2d6d8cc23d53ac2b6254dd3
DashProsh <dash.prosh.am@gmail.com>
2024-10-23 19:24:51 +0300

    Dependency update and commit changes

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("application")
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+application {
+    mainClass.set("org.example.Main")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
